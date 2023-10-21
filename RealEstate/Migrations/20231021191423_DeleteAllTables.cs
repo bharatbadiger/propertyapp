f7071e49a558e3eb6a2055847085f6bd225dbf41
Bharat Badiger <bharat.badiger@energyexemplar.com>
2023-10-22 00:45:27 +0530

    Added Delete Tables

---
+++ RealEstate/Migrations/20231021191423_DeleteAllTables.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstate.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteAllTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DECLARE @sql NVARCHAR(MAX) = N'';\n" +
+                             "SELECT @sql += 'DROP TABLE ' + QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME) + ';' FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE';\n" +
+                             "EXEC(@sql);");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Optionally, you can provide a method to revert the migration, but it's not possible to automatically recreate the tables with EF Core migrations.
+        }
+
+    }
+}
