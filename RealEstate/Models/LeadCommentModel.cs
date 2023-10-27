92a4088068e062fd0b81e49789dde9964960157f
Bharat Badiger <bharat.badiger@energyexemplar.com>
2023-10-27 14:14:30 +0530

    Add key in Comment model

--- RealEstate/Models/LeadCommentModel.cs
+++ RealEstate/Models/LeadCommentModel.cs
@@ -4,6 +4,7 @@ namespace RealEstate.Models
 {
     public class LeadCommentModel
     {
+        public Guid Id { get; set; }
         public virtual string? Comment { get; set; }
         public virtual UserType UserType { get; set; }
         public virtual DateTimeOffset? TimeStamp { get; set; }
