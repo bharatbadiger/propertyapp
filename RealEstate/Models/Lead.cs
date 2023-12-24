37a1336ca975f137e4cfe8fcb03179a8c58162ed
Bharat Badiger <41826488+bharatbadiger@users.noreply.github.com>
2023-12-24 22:56:24 +0530

    Update Lead.cs

--- RealEstate/Models/Lead.cs
+++ RealEstate/Models/Lead.cs
@@ -8,7 +8,8 @@ namespace RealEstate.Models
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public virtual LeadPropertyType PropertyType { get; set; }
+        public virtual LeadPropertyType LeadPropertyType { get; set; }
+        public virtual PropertyType PropertyType { get; set; }
         public virtual string MobileNo { get; set; }
         public virtual string FullName { get; set; }
         public virtual string Status { get; set; } // Open or Closed
