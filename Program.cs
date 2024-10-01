acaa80031c2fb1e810ae4dfd4af4c84d8e15571a
Bharat Badiger <41826488+bharatbadiger@users.noreply.github.com>
2024-10-01 18:14:52 +0530

    Allow All Origins

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.WithOrigins("*")
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
